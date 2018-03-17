d604e0a7809f408fd480d8b91ee98917f1950039
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-17 22:01:36 +0800

    Add muse-ui.

--- client/app.js
+++ client/app.js
@@ -3,8 +3,12 @@ import { sync } from 'vuex-router-sync'
 import App from './components/App'
 import router from './router'
 import store from './store'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
 
 sync(store, router)
+
+Vue.use(MuseUI)
 
 const app = new Vue({
   router,
