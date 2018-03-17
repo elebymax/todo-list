ba189e51ed3ea9d9c2d2b1e404cd50dcb1ef7121
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-17 22:59:20 +0800

    Add the component DailyBox.

---
+++ client/components/DailyBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-daily-box">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: ""
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .my-daily-box {
+    padding: 8px;
+
+    h2 {
+      border-bottom: 1px solid #eee;
+      padding-bottom: 8px;
+    }
+  }
+</style>
