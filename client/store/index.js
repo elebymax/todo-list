f0dc01edc4f186c3a6a06776ace907e9814ea20e
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 12:35:14 +0800

    Add the feature of getting todos by date in vuex.

--- client/store/index.js
+++ client/store/index.js
@@ -4,19 +4,36 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  todoIndexCount: 1,
+  todoIndexCount: 2,
   todos: [
     {
       id: 1,
       date: '2018-03-18',
       text: '跟朋友吃飯',
       done: false
+    },
+    {
+      id: 2,
+      date: '2018-03-18',
+      text: '跟朋友吃飯2',
+      done: false
     }
   ]
 }
 
 const getters = {
+  todosByDate: (state) => (date) => {
+    let todos = [];
 
+    for(let i=0; i<state.todos.length; i++) {
+      const item = state.todos[i];
+      if (item.date === date) {
+        todos.push(item);
+      }
+    }
+
+    return todos;
+  }
 }
 
 const mutations = {
