af6d4c419006d6a15a482b86a7d7a7516c14b61d
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 12:36:05 +0800

    Add EditBox and ItemBox to DailyBox.

--- client/components/DailyBox.vue
+++ client/components/DailyBox.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="my-daily-box">
     <h2>{{ title }}</h2>
+    <div class="my-daily-box-content">
+      <item-box v-for="(item, index) in todosByDate(date)"
+                :key="item.id"
+                :text="item.text"></item-box>
+      <edit-box hintText="請輸入事項"></edit-box>
+    </div>
   </div>
 </template>
 <script>
+  import { mapGetters, mapMutations } from 'vuex';
+  import EditBox from '../components/EditBox';
+  import ItemBox from '../components/ItemBox';
+
   export default {
+    computed: {
+      ...mapGetters([
+        'todosByDate'
+      ])
+    },
     props: {
       title: {
         type: String,
         default: ""
+      },
+      date: {
+        type: String,
+        default: ""
       }
+    },
+    data() {
+      return {
+        
+      }
+    },
+    components: {
+      EditBox,
+      ItemBox
     }
   }
 </script>
@@ -20,6 +48,10 @@
     h2 {
       border-bottom: 1px solid #eee;
       padding-bottom: 8px;
+    }
+
+    .my-daily-box-content {
+
     }
   }
 </style>
