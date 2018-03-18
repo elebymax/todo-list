d28baf97b88773e408ed079d02cfa4bd1c3d3ded
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 17:01:28 +0800

    Add DailyBox in to Home.

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="page">
-    <nav-bar></nav-bar>
+    <nav-bar title="Eleby's Todos"></nav-bar>
+    <div id="container">
+      <daily-box title="Day1" date="2018-03-18"></daily-box>
+    </div>
   </div>
 </template>
 
 <script>
   import NavBar from '../components/NavBar';
+  import DailyBox from '../components/DailyBox';
 
   export default {
     components: {
-      NavBar
+      NavBar,
+      DailyBox
     }
   }
 </script>
 <style lang="scss" scoped>
-
+  #container {
+    padding: 12px 36px;
+  }
 </style>
