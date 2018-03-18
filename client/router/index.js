c0406a860bb3f04a6714f1282756707c247beb67
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 19:03:56 +0800

    Add Todos page and props.

--- client/router/index.js
+++ client/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home'
 import Daily from '../views/Daily'
+import Todos from '../views/Todos'
 
 Vue.use(Router)
 
@@ -12,7 +13,27 @@ export default new Router({
       path: '/',
       component: Home,
       children: [
-        { path: 'daily-todos', component: Daily }
+        {
+          path: 'daily-todos',
+          component: Daily,
+          props: {
+            mode: 'daily-mode'
+          }
+        },
+        {
+          path: 'done-todos',
+          component: Todos,
+          props: {
+            mode: 'done-mode'
+          }
+        },
+        {
+          path: 'undone-todos',
+          component: Todos,
+          props: {
+            mode: 'undone-mode'
+          }
+        },
       ]
     }
   ]
