3e3fa9f5d1f40a07fa567a991c38ea72859d01b5
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 13:50:27 +0800

    Add the component ItemBoxesWrapper.

---
+++ client/components/ItemBoxesWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-item-boxes-wrapper">
+    <h3>{{ title }}</h3>
+    <div class="my-item-boxes-wrapper-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: ""
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .my-item-boxes-wrapper {
+    margin-top: 24px;
+    padding-left: 18px;
+    box-sizing: border-box;
+
+    .my-item-boxes-wrapper-content {
+      padding-left: 18px;
+      box-sizing: border-box;
+    }
+  }
+</style>
