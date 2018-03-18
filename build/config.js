d2481066740fb60fde8e92affb90e59d8505fa5a
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 19:17:15 +0800

    Fix the public path.

--- build/config.js
+++ build/config.js
@@ -11,5 +11,5 @@ module.exports = {
   },
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
-  publicPath: '/',
+  publicPath: './',
 }
