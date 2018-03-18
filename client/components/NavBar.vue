aa961cd6ca1b009b08f5113eb60049018947f058
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 16:47:33 +0800

    Fix title for NavBar.

--- client/components/NavBar.vue
+++ client/components/NavBar.vue
@@ -1,11 +1,18 @@
 <template>
-  <mu-appbar title="Title">
+  <mu-appbar :title="title">
     <mu-text-field icon="search" class="navbar-search-field" slot="right" hintText="請輸入搜尋內容"/>
     <mu-flat-button color="white" label="flat Button" slot="right"/>
   </mu-appbar>
 </template>
 <script>
-
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: ""
+      }
+    }
+  }
 </script>
 <style lang="scss">
   .navbar-search-field {
