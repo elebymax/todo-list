85c0f6ec74c53919ef8e1a9f3b420c7a2e7e8e4d
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 13:51:00 +0800

    Add ItemBoxesWrapper to wrap the ItemBox.

--- client/components/DailyBox.vue
+++ client/components/DailyBox.vue
@@ -2,11 +2,23 @@
   <div class="my-daily-box">
     <h2>{{ title }}</h2>
     <div class="my-daily-box-content">
-      <item-box v-for="(item, index) in todosByDate(date)"
-                :key="item.id"
-                :id="item.id"
-                :text="item.text"></item-box>
+      <item-boxes-wrapper title="未完成">
+        <item-box v-for="(item, index) in undoneTodosByDate(date)"
+                  :isChecked="false"
+                  :key="item.id"
+                  :id="item.id"
+                  :text="item.text"></item-box>
+      </item-boxes-wrapper>
+
       <edit-box hintText="請輸入事項"></edit-box>
+
+      <item-boxes-wrapper title="已完成">
+        <item-box v-for="(item, index) in doneTodosByDate(date)"
+                  :isChecked="true"
+                  :key="item.id"
+                  :id="item.id"
+                  :text="item.text"></item-box>
+      </item-boxes-wrapper>
     </div>
   </div>
 </template>
@@ -14,11 +26,13 @@
   import { mapGetters, mapMutations } from 'vuex';
   import EditBox from '../components/EditBox';
   import ItemBox from '../components/ItemBox';
+  import ItemBoxesWrapper from '../components/ItemBoxesWrapper';
 
   export default {
     computed: {
       ...mapGetters([
-        'todosByDate'
+        'doneTodosByDate',
+        'undoneTodosByDate'
       ])
     },
     props: {
@@ -38,7 +52,8 @@
     },
     components: {
       EditBox,
-      ItemBox
+      ItemBox,
+      ItemBoxesWrapper
     }
   }
 </script>
@@ -53,6 +68,9 @@
 
     .my-daily-box-content {
 
+      .my-daily-box-item-box-wrapper {
+        margin-top: 24px;
+      }
     }
   }
 </style>
