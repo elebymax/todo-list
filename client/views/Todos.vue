e33ee93d99fe44fd98a7c1611dedea3e253213be
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-18 19:06:03 +0800

    Add the view component Todos.

---
+++ client/views/Todos.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="todos-container" class="container">
+    <daily-box v-for="(item, index) in dateArr"
+               :key="item.id"
+               :date="item"
+               :mode="mode"></daily-box>
+  </div>
+</template>
+<script>
+  import { mapGetters, mapMutations } from 'vuex';
+  import DailyBox from '../components/DailyBox';
+
+  export default {
+    watch: {
+      todoIndexCount () {
+        console.log("value change");
+        this.reloadPage();
+      },
+      '$route' () {
+        this.reloadPage();
+      }
+    },
+    mounted() {
+      this.reloadPage();
+    },
+    props: {
+      mode: {
+        type: String,
+        default: ""
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'isDoneDateArr',
+        'todoIndexCount'
+      ]),
+      resetDate() {
+        return new Date().toISOString().slice(0,10);
+      }
+    },
+    data() {
+      return {
+        dateArr: []
+      }
+    },
+    methods: {
+      reloadPage() {
+        if (this.mode === 'done-mode') {
+          this.dateArr = this.isDoneDateArr(true);
+        } else if (this.mode === 'undone-mode') {
+          this.dateArr = this.isDoneDateArr(false);
+        }
+      }
+    },
+    components: {
+      DailyBox
+    }
+  }
+</script>
+<style lang="scss">
+  .container {
+    padding: 12px 36px;
+  }
+</style>
