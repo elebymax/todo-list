4d87fcf68086aa8c3f33d3b3e42a80ccde9750d2
Lai chane an <m020203@gms.tku.edu.tw>
2018-03-19 13:19:16 +0800

    Add RWD feature for NavBar.

--- client/components/NavBar.vue
+++ client/components/NavBar.vue
@@ -1,16 +1,45 @@
 <template>
   <mu-appbar :title="title">
-    <mu-flat-button color="white" label="已完成項目" slot="right" @click="handleShowDoneTodosClicked"/>
-    <mu-flat-button color="white" label="未完成項目" slot="right" @click="handleShowUndoneTodosClicked"/>
-    <mu-flat-button color="white" label="單日項目" slot="right" @click="handleShowDailyTodosClicked"/>
+    <mu-flat-button v-if="!isSmallDevice" color="white" label="已完成項目" slot="right" @click="handleShowDoneTodosClicked"/>
+    <mu-flat-button v-if="!isSmallDevice" color="white" label="未完成項目" slot="right" @click="handleShowUndoneTodosClicked"/>
+    <mu-flat-button v-if="!isSmallDevice" color="white" label="單日項目" slot="right" @click="handleShowDailyTodosClicked"/>
+    <mu-icon-menu
+      icon="more_vert"
+      slot="right"
+      v-if="isSmallDevice"
+      :anchorOrigin="rightTop"
+      :targetOrigin="rightTop">
+      <mu-menu-item title="已完成項目" @click="handleShowDoneTodosClicked"/>
+      <mu-menu-item title="未完成項目" @click="handleShowUndoneTodosClicked"/>
+      <mu-menu-item title="單日項目" @click="handleShowDailyTodosClicked"/>
+    </mu-icon-menu>
   </mu-appbar>
 </template>
 <script>
   export default {
+    mounted() {
+      let vue = this;
+      this.$nextTick(function() {
+        window.addEventListener('resize', function(e) {
+          vue.windowWidth = window.innerWidth
+        });
+      })
+    },
+    computed: {
+      isSmallDevice() {
+        return this.windowWidth < 768
+      }
+    },
     props: {
       title: {
         type: String,
         default: ""
+      }
+    },
+    data() {
+      return {
+        windowWidth: 0,
+        rightTop: {horizontal: 'right', vertical: 'top'}
       }
     },
     methods: {
